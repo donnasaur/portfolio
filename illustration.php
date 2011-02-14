1ae934187697060d49e993881592035db0183e97
Donna Almendrala <donna.almendrala@gmail.com>
2011-02-14 01:20:17 -0500

    changed illustration order

--- illustration.php
+++ illustration.php
@@ -11,8 +11,8 @@ Copyright 2011 Donna Almendrala (see AUTHORS)
 <?php include("includes/header.php"); ?>
 			<div id="content">
 				<h6 class="clear">Illustration</h6>
-				<div class="thumbnail"><a href="images/mountaingoats.png" rel="lightbox[illustration]" title="Faux Mountain Goats Flyer"><img src="images/mountaingoats_thumb.png" height="46px" width="42px" alt="Faux Mountain Goats Flyer" /></a></div>
 				<div class="thumbnail"><a href="images/strangeromance.png" rel="lightbox[illustration]" title="Faux 1970's Romance Cover"><img src="images/strangeromance_thumb.png" height="46px" width="42px" alt="Faux 1970's Romance Cover" /></a></div>
+				<div class="thumbnail"><a href="images/mountaingoats.png" rel="lightbox[illustration]" title="Faux Mountain Goats Flyer"><img src="images/mountaingoats_thumb.png" height="46px" width="42px" alt="Faux Mountain Goats Flyer" /></a></div>
 				<div class="thumbnail"><a href="images/screenprint_bio.png" rel="lightbox[illustration]" title="Screenprinted Portrait"><img src="images/screenprint_bio_thumb.png" height="46px" width="42px" alt="Screenprinted Portrait" /></a></div>
 				<div class="thumbnail"><a href="images/bearjordan.png" rel="lightbox[illustration]" title="Bear Jordan"><img src="images/bearjordan_thumb.png" height="46px" width="42px" alt="Bear Jordan" /></a></div>
 			</div>
